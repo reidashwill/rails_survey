b3c807785fe18cc95ab7f95cabd95f408a52a0e0
Reid Ashwill <reidashwill@gmail.com>
2020-05-27 16:12:03 -0700

    troubleshoot multiple choice answers

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,22 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
+
+  before_save(:titleize_survey)
+
+  private
+  def titleize_survey
+    no_caps = ["a", "is", "you", "he", "she", "it", "we", "you", "me", "him", "her", "us", "my",  "his", "its", "our", "who",  "if", "how", "as"]
+    result = []
+    split_name = self.name.split(" ")
+    split_name.each do |w|
+      if no_caps.any? {|c| w == c }
+        w = w
+        result.push(w)
+      else
+       w = w.titleize
+       result.push(w)
+      end
+    end  
+    self.name = result.join(" ")
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 describe Survey do
   it { should have_many(:questions) }
 end
+
+describe Survey do
+  it("titleizes a survey") do
+    survey = Survey.create({name: "this is a test"})
+    expect(survey.name()).to(eq("This is a Test"))
+  end
+end
